87423f12ad1f56d5f9c878d77ddf2df4ba320a9c
Nikhil Jain <nikhilpjain1993@gmail.com>
2024-01-24 10:20:39 +0530

    Initial Cmake file added

--- LightYears/src/main.cpp
+++ LightYears/src/main.cpp
@@ -1,8 +1,23 @@
 #include<iostream>
+#include<SFML/Graphics.hpp>
+
 using namespace std;
 
 int main()
 {
+	sf::RenderWindow renderWindow{ sf::VideoMode(100,100), "MyWindow"};
+
+	while (renderWindow.isOpen())
+	{
+		sf::Event windowEvent;
+		while (renderWindow.pollEvent(windowEvent))
+		{
+			if (windowEvent.type == sf::Event::EventType::Closed)
+			{
+				renderWindow.close();
+			}
+		}
+	}
 	cout << "Hello world" << endl;
 	return 0;
 }
